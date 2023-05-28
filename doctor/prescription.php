b02d7768211aebe8745544120ba39e0b458fa0f4
aghazeeshan <zeeshan107526@gmail.com>
2023-05-28 15:50:49 +0500

    third commit

---
+++ doctor/prescription.php
@@ -0,0 +1,184 @@
+<!-- <i class="fa fa-arrow-circle-left" aria-hidden="true"></i> -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Learning Style</title>
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/cust_images/logo_blue.svg">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Developer : Salman agha (Whatsapp : +923347360763, aghasalmankhan95@gmail.com)" name="description" />
+    <meta content="Mannatthemes" name="author" />
+    <!--Morris Chart CSS -->
+    <link rel="stylesheet" href="assets/plugins/morris/morris.css">
+    <link href="assets/plugins/sweet-alert2/sweetalert2.min.css" rel="stylesheet" type="text/css">
+    <!-- App css -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/icons.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/metisMenu.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.0/mdb.min.css"
+    rel="stylesheet"
+    />
+</head>
+<body>
+ <div class="page-wrapper"> 
+    <div class="page-content">
+        <div class="container-fluid">
+
+
+
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h1>Prescription</h1>
+                </div>
+            </div>
+
+
+            <div class="row mt-5 mb-5">
+                <div class="col-md-2"></div>
+                <div class="col-md-8 mt-5 mb-5">
+                    <div class="card">
+                        <div class="card-body">
+
+
+                            <h3>Patient ID</h3>
+                            <div class="form-container">
+                                <form class="bill-form">
+                                    <div class="row">
+                                        <div class="col-md-6">                            
+                                            <div class="form-group">
+                                                <label>Full Name</label>
+                                                <input placeholder="Full name" class="form-control" type="text" name="name">
+                                            </div>
+                                        </div><!--end col-->
+                                        <div class="col-md-6">                            
+                                            <div class="form-group">
+                                                <label>Mobile Number</label>
+                                                <input placeholder="Mobile number" class="form-control" type="tel" name="number">
+                                            </div>
+                                        </div><!--end col-->
+                                    </div><!--end row-->
+                                    <div class="row">
+                                        <div class="col-md-6">                            
+                                            <div class="form-group">
+                                                <label>Age</label>
+                                                <input placeholder="Age" class="form-control" type="number" name="nubmer">
+                                            </div>
+                                        </div><!--end col-->
+                                        <div class="col-md-6">                            
+                                            <div class="form-group">
+                                                <label>Address</label>
+                                                <input placeholder="Address" class="form-control" type="text" name="name">
+                                            </div>
+                                        </div><!--end col-->
+                                        <div class="col-md-6">                            
+                                            <div class="form-group">
+                                                <label>Sex</label>
+                                                <select class="form-select" aria-label="Default select example">
+                                                  <option selected>Sex</option>
+                                                  <option value="1">Male</option>
+                                                  <option value="2">Female</option>
+                                              </select>
+                                          </div>
+                                      </div><!--end col-->
+                                      <div class="col-md-6">                            
+                                      </div><!--end col-->
+                                      <div class="col-md-6">                            
+                                        <div class="form-group">
+                                            <label>Chief Compliante</label>
+                                            <input placeholder="Chief Compliante" class="form-control" type="text" name="">
+                                        </div>
+                                    </div><!--end col-->
+                                    <div class="col-md-6">                            
+                                    </div><!--end col-->
+                                    <div class="col-md-6">                            
+                                        <div class="form-group">
+                                            <label>Daignosis</label>
+                                            <input placeholder="Daignosis" class="form-control" type="text" name="Daignosis">
+                                        </div>
+                                    </div><!--end col-->
+                                </div><!--end row-->
+                                <!-- <button class="btn btn-success px-3">Pay Now</button> -->
+                            </form><!--end form-->
+                        </div><!--end form-container-->
+
+<h3>Rx</h3>
+                        <table class="table">
+                          <thead>
+                            <tr>
+                              <th scope="col">#</th>
+                              <th scope="col">First</th>
+                              <th scope="col">Last</th>
+                              <th scope="col">Handle</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th scope="row">1</th>
+                          <td>Mark</td>
+                          <td>Otto</td>
+                          <td>@mdo</td>
+                      </tr>
+                      <tr>
+                          <th scope="row">2</th>
+                          <td>Jacob</td>
+                          <td>Thornton</td>
+                          <td>@fat</td>
+                      </tr>
+                      <tr>
+                          <th scope="row">3</th>
+                          <td colspan="2">Larry the Bird</td>
+                          <td>@twitter</td>
+                      </tr>
+                  </tbody>
+              </table>
+
+
+          </div><!--end card-body-->
+      </div><!--end card-->
+  </div><!--end col-->
+  <div class="col-md-2"></div>
+</div><!--end row-->                                                   
+
+
+
+</div>
+</div>                       
+<!-- end page content -->
+</div>
+<!-- end page-wrapper -->
+<!-- jQuery  -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/metisMenu.min.js"></script>
+<script src="assets/js/waves.min.js"></script>
+<script src="assets/js/jquery.slimscroll.min.js"></script>
+<script src="../assets/js/jquery.core.js"></script>
+<!--Plugins-->
+<script src="assets/plugins/moment/moment.js"></script>
+<!-- Required datatable js -->
+<script src="assets/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="assets/plugins/datatables/dataTables.bootstrap4.min.js"></script>
+<script src="assets/plugins/datatables/dataTables.responsive.min.js"></script>
+<script src="assets/plugins/datatables/responsive.bootstrap4.min.js"></script>
+<script src="assets/plugins/sweet-alert2/sweetalert2.min.js"></script>
+<script src="assets/pages/jquery.sweet-alert.init.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+<script src="../assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/metisMenu.min.js"></script>
+<script src="assets/js/waves.min.js"></script>
+<script src="assets/js/jquery.slimscroll.min.js"></script>
+<script src="assets/js/jquery.core.js"></script>
+<script src="assets/js/app.js"></script>
+<script src="functions/Staff.js"></script>
+<!-- App js -->
+<script src="assets/js/app.js"></script>
+</body>
+</html>
